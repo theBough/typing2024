02e958e3e22240c8310791465788192f29f31478
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-09-17 13:35:34 -0500

    Update player.js

--- player.js
+++ player.js
@@ -8,11 +8,16 @@ function Player(x,y,w,h,col){
   this.display = function(){
     push()
     fill(this.col)
+    console.log("test")
     ellipse(this.x, this.y, this.w, this.h)
     pop()
   }
   this.update = function(){
-    
+    //look to see if the user presses a key
+    if(keyIsDown(37)){
+      //the left arrow key
+      this.x -= 5
+    }
   }
   
 }
