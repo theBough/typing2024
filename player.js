c43359e03a2e13b6c606d4ee8316d2c71b6f71fa
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-09-17 13:24:39 -0500

    Update player.js

--- player.js
+++ player.js
@@ -6,7 +6,10 @@ function Player(x,y,w,h,col){
   this.col = col;
   
   this.display = function(){
-    
+    push()
+    fill(this.col)
+    ellipse(this.x, this.y, this.w, this.h)
+    pop()
   }
   this.update = function(){
     
