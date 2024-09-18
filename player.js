bf3451f09020dbb2716c501f20f7d3a867baacfa
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-09-18 13:23:01 -0500

    Update player.js

--- player.js
+++ player.js
@@ -8,7 +8,6 @@ function Player(x,y,w,h,col){
   this.display = function(){
     push()
     fill(this.col)
-    console.log("test")
     ellipse(this.x, this.y, this.w, this.h)
     pop()
   }
@@ -17,6 +16,18 @@ function Player(x,y,w,h,col){
     if(keyIsDown(37)){
       //the left arrow key
       this.x -= 5
+    }
+    if(keyIsDown(38)){
+      //the up arrow key
+      this.y -= 5
+    }
+    if(keyIsDown(39)){
+      //the right arrow key
+      this.x += 5
+    }
+    if(keyIsDown(40)){
+      //the down arrow key
+      this.y += 5
     }
   }
   
